6855c6bc81803b4e4190a4aaaa87166f4f138b76
danhdeng <hui_danny_deng@hotmail.com>
2022-01-11 23:22:02 -0500

    update app.ts to remove unneed code

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ import setResponseHeader from './middleware/setResponseHeader';
 import cors from 'cors';
 import config from 'config';
 import cookieParser from 'cookie-parser';
-import { shouldSendSameSiteNone } from 'should-send-same-site-none';
+// import { shouldSendSameSiteNone } from 'should-send-same-site-none';
 
 class App {
     public express: Application;
@@ -21,37 +21,37 @@ class App {
     }
     private initializeMiddleware() {
         //this.express.use(setResponseHeader);
-        this.express.set('trust proxy', 1);
+        //this.express.set('trust proxy', 1);
         this.express.use(
             cors({
-                origin: 'http://localhost:3000',
+                origin: config.get('origin'),
                 credentials: true,
             })
         );
-        this.express.use(function (req, res, next) {
-            // Website you wish to allow to connect
-            res.setHeader(
-                'Access-Control-Allow-Origin',
-                'http://localhost:3000'
-            );
-            // Request methods you wish to allow
-            res.setHeader(
-                'Access-Control-Allow-Methods',
-                'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-            );
-            // Request headers you wish to allow
-            res.setHeader(
-                'Access-Control-Allow-Headers',
-                'Origin,X-Requested-With,content-type,set-cookie'
-            );
-            // Set to true if you need the website to include cookies in the requests sent
-            // to the API (e.g. in case you use sessions)
-            res.setHeader('Access-Control-Allow-Credentials', 'true');
+        // this.express.use(function (req, res, next) {
+        //     // Website you wish to allow to connect
+        //     res.setHeader(
+        //         'Access-Control-Allow-Origin',
+        //         'http://localhost:3000'
+        //     );
+        //     // Request methods you wish to allow
+        //     res.setHeader(
+        //         'Access-Control-Allow-Methods',
+        //         'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+        //     );
+        //     // Request headers you wish to allow
+        //     res.setHeader(
+        //         'Access-Control-Allow-Headers',
+        //         'Origin,X-Requested-With,content-type,set-cookie'
+        //     );
+        //     // Set to true if you need the website to include cookies in the requests sent
+        //     // to the API (e.g. in case you use sessions)
+        //     res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-            // Pass to next layer of middleware
-            next();
-        });
-        this.express.use(shouldSendSameSiteNone);
+        //     // Pass to next layer of middleware
+        //     next();
+        // });
+        // this.express.use(shouldSendSameSiteNone);
         this.express.use(cookieParser());
         this.express.use(express.json());
         this.express.use(deserializeUser);
